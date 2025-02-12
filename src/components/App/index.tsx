ac4740e28a067a39e01cb0dec37946a92d42a05a
devdukalis <devdukalis@gmail.com>
2025-02-12 14:45:57 +0300

    feat: add antd

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "antd"
+
 function App() {
-  return <h1>Vite + React + TS</h1>
+  return (
+    <div className="App">
+      <h1>Vite + React + TS</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
 
 export default App
