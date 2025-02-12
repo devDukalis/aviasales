ce55ae66b1e0cf1b1e072c1ebca59d86fbce3424
devdukalis <devdukalis@gmail.com>
2025-02-12 12:44:28 +0300

    feat: init

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint, { configs as tseslintConfigs } from "typescript-eslint"
+import reactPlugin from "eslint-plugin-react"
+import importPlugin from "eslint-plugin-import"
+import jsxA11y from "eslint-plugin-jsx-a11y"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+
+const project = "./tsconfig.json"
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules", "build"] },
+  {
+    extends: [
+      js.configs.recommended,
+      tseslintConfigs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      jsxA11y.flatConfigs.recommended,
+      eslintPluginPrettier,
+    ],
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project,
+        },
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/prop-types": "off",
+      indent: ["error", 2],
+      "linebreak-style": [0, "unix"],
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      semi: ["error", "never"],
+      "import/no-unresolved": [2, { caseSensitive: false }],
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+      "import/order": [
+        2,
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+)
